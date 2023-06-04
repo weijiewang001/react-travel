126ac031a17e2d8f70e94fc68ea5bd1cdfe9225c
wayne <weijiewang001@gmail.com>
2023-06-05 00:43:06 +1000

    react-redux function和class

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -4,15 +4,39 @@ import logo from '../../assets/logo.svg';
 import { Layout, Typography, Input, Menu, Button, Dropdown } from "antd";
 import { GlobalOutlined } from "@ant-design/icons";
 import { useParams, useLocation, useNavigate } from "react-router-dom"
+// import { useSelector } from "react-redux";
+// import { RootState } from "../../redux/store"
+import { useSelector } from '../../redux/hooks';
+import { useDispatch } from 'react-redux';
+import { Dispatch } from "redux";
+import { LanguageActionTypes, addLanguageActionCreator, changeLanguageActionCreator } from '../../redux/language/languageActions';
+import { useTranslation } from "react-i18next"
 
 
 export const Header: React.FC = () => {
   const navigate = useNavigate();
   const location = useLocation();
   const params = useParams();
+
+  const language = useSelector((state) => state.language);
+  const languageList = useSelector(state => state.languageList);
+  const dispatch = useDispatch();
+  // 引入i18n组件
+  const { t } = useTranslation();
+
   // console.log("navigate", navigate)
   // console.log("location", location)
   // console.log("params", params)
+
+  const menuClickHandler = (e) => {
+    console.log(e);
+    if (e.key === "new") {
+      //处理新语言添加的action
+      dispatch(addLanguageActionCreator("新语言", "new_lang"))
+    } else {
+      dispatch(changeLanguageActionCreator(e.key))
+    }
+  };
 
 
   return (
@@ -20,24 +44,23 @@ export const Header: React.FC = () => {
       {/* top-header */}
       <div className={styles['top-header']}>
         <div className={styles.inner}>
-          <Typography.Text>让旅游更幸福</Typography.Text>
+          <Typography.Text>{t("header.slogan")}</Typography.Text>
           <Dropdown.Button
             style={{ marginLeft: 15, display: "inline" }}
             overlay={
-              <Menu
-                items={[
-                  { key: "1", label: "中文" },
-                  { key: "2", label: "English" },
-                ]}
+              <Menu onClick={menuClickHandler}
+                items={[...languageList.map((l) => {
+                  return { key: l.code, label: l.name };
+                }), { key: "new", label: `${t("header.add_new_language")}` }]}
               />
             }
             icon={<GlobalOutlined />}
           >
-            语言
+            {language === "en" ? "English" : "中文"}
           </Dropdown.Button>
           <Button.Group className={styles['button-group']}>
-            <Button onClick={() => navigate("/register")}>注册</Button>
-            <Button onClick={() => navigate("/signin")}>登录</Button>
+            <Button onClick={() => navigate("/register")}>{t("header.register")}</Button>
+            <Button onClick={() => navigate("/signin")}>{t("header.signin")}</Button>
           </Button.Group>
         </div>
       </div>
@@ -46,7 +69,7 @@ export const Header: React.FC = () => {
           <img src={logo} alt="logo" className={styles['App-logo']}
 
           />
-          <Typography.Title level={3} className={styles.title}>React 旅游网</Typography.Title>
+          <Typography.Title level={3} className={styles.title}>{t("header.title")}</Typography.Title>
         </span>
         <Input.Search
           placeholder="请输入旅游目的地，主题，或者关键字"
@@ -55,22 +78,22 @@ export const Header: React.FC = () => {
       </Layout.Header>
       <Menu mode={"horizontal"} className={styles['main-menu']}
         items={[
-          { key: "1", label: "旅游首页" },
-          { key: "2", label: "周末游" },
-          { key: "3", label: "跟团游" },
-          { key: "4", label: "自由行" },
-          { key: "5", label: "私家团" },
-          { key: "6", label: "邮轮" },
-          { key: "7", label: "酒店+景点" },
-          { key: "8", label: "当地玩乐" },
-          { key: "9", label: "主题游" },
-          { key: "10", label: "定制游" },
-          { key: "11", label: "游学" },
-          { key: "12", label: "签证" },
-          { key: "13", label: "企业游" },
-          { key: "14", label: "高端游" },
-          { key: "15", label: "爱玩户外" },
-          { key: "16", label: "保险" },
+          { key: "1", label: t("header.home_page") },
+          { key: "2", label: t("header.weekend") },
+          { key: "3", label: t("header.group") },
+          { key: "4", label: t("header.backpack") },
+          { key: "5", label: t("header.private") },
+          { key: "6", label: t("header.cruise") },
+          { key: "7", label: t("header.hotel") },
+          { key: "8", label: t("header.local") },
+          { key: "9", label: t("header.theme") },
+          { key: "10", label: t("header.custom") },
+          { key: "11", label: t("header.study") },
+          { key: "12", label: t("header.visa") },
+          { key: "13", label: t("header.enterprise") },
+          { key: "14", label: t("header.high_end") },
+          { key: "15", label: t("header.outdoor") },
+          { key: "16", label: t("header.insurance") },
         ]}
       >
 
